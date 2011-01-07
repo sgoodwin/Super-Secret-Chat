5ede05a251e387eb3c7b4711ab0718bfa69bb5bd
Samuel Goodwin <goodwinlabs@gmail.com>
2011-01-07 13:08:53 -0700

    Think I got this.

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8124);
+}).listen(process.env.PORT || 8001);
